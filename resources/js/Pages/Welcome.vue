40e6f6014124230929a0bf0546821545c6ab5261
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2022-02-16 22:49:01 -0500

    Conoce y usa Laravel Jetstream

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+            Welcome3
+        </div>
+    </div>
+</template>
+
+<script>
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        components: {
+            //
+        },
+
+        props: {
+            canLogin: Boolean,
+            canRegister: Boolean,
+        }
+    })
+</script>
